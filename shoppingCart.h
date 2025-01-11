264e0dd44cb767bd87cfab053588864f97c5e69c
MatanYZ1818 <matniredon10@Gmail.com>
2025-01-11 18:55:43 +0200

    ......

--- shoppingCart.h
+++ shoppingCart.h
@@ -12,15 +12,4 @@ float calculateTotal(const ShoppingCart* cart);
 void printCart(const ShoppingCart* s);
 void freeCart(ShoppingCart* pCart);
 
-typedef struct {
-	char* barcode[BARCODE_LEN];
-	float price;
-	int amount;
-	ShoppingItem** itemList;
-} ShoppingCart;
-
-int calculatePrice(const ShoppingItem* item);
-void initItem(Product* p, ShoppingItem* item);
-void addItem(ShoppingItem** item, ShoppingCart* cart);
-void printCart(ShoppingCart* s);
 
